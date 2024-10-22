f01c19aef0971300b525f5bf901d440a5ab4bf43
Dhirajsingh212 <dhirajsingh0229@gmail.com>
2024-10-22 23:20:57 +0530

    minor updates

--- web/client/app/(dashboard)/layout.tsx
+++ web/client/app/(dashboard)/layout.tsx
@@ -1,8 +1,13 @@
-import { ReactNode } from "react"
+import AppSidebar from '@/components/AppSidebar'
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import { ReactNode } from 'react'
 
-const Layout = ({children}:{children:ReactNode}) => {
+const Layout = ({ children }: { children: ReactNode }) => {
   return (
-      <div>{ children}</div>
+    <SidebarProvider>
+      <AppSidebar />
+      <main className='p-4'>{children}</main>
+    </SidebarProvider>
   )
 }
 
