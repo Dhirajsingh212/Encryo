96144a5d2f7a3ce8d5be1a0f0776af76b277ec05
Dhirajsingh212 <dhirajsingh0229@gmail.com>
2024-10-26 17:12:05 +0530

    minor updates

--- web/client/app/(dashboard)/home/[...id]/page.tsx
+++ web/client/app/(dashboard)/home/[...id]/page.tsx
@@ -1,10 +1,12 @@
 import { getEnvsByProjectSlug } from '@/actions/saveEnvs'
 import HomePageCreateMenu from '@/components/HomePageCreateMenu'
 import SettingsComp from '@/components/SettingsComp'
+import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { auth } from '@clerk/nextjs/server'
 import { Key, Search, User } from 'lucide-react'
+import { FaTrash } from 'react-icons/fa'
 
 const Page = async ({ params }: { params: { id: string[] } }) => {
   const { userId } = auth()
@@ -35,33 +37,39 @@ const Page = async ({ params }: { params: { id: string[] } }) => {
               <HomePageCreateMenu />
             </div>
           </div>
-          <div>
+          <div className='flex flex-col gap-4'>
             {projectDetails &&
               projectDetails.envs &&
               projectDetails.envs.map((pairs, index) => {
                 return (
                   <div
                     key={index}
-                    className='grid grid-cols-1 gap-4 md:grid-cols-2'
+                    className='flex flex-col items-center gap-4 py-2 md:flex-row'
                   >
-                    <div className='relative'>
+                    <div className='relative w-full'>
                       <User className='absolute left-3 top-1/2 h-5 w-5 -translate-y-1/2 text-gray-400' />
                       <Input
                         disabled
                         placeholder='Name'
                         value={pairs.name}
-                        className='border-2 bg-card pl-10 pr-4 dark:border-input dark:bg-neutral-900'
+                        className='w-full border-2 bg-card pl-10 pr-4 dark:border-input dark:bg-neutral-900'
                       />
                     </div>
-                    <div className='relative'>
+                    <div className='relative w-full'>
                       <Key className='absolute left-3 top-1/2 h-5 w-5 -translate-y-1/2 text-gray-400' />
                       <Input
                         disabled
                         placeholder='API KEY'
                         value={pairs.value}
-                        className='border-2 bg-card pl-10 pr-4 dark:border-input dark:bg-neutral-900'
+                        className='w-full border-2 bg-card pl-10 pr-4 dark:border-input dark:bg-neutral-900'
                       />
                     </div>
+                    <Button
+                      variant='destructive'
+                      className='mt-2 w-full md:mt-0 md:w-10'
+                    >
+                      <FaTrash />
+                    </Button>
                   </div>
                 )
               })}
