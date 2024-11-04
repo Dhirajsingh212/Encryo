2d7cc52a6cb42aa32ee415555665959ddbe000ff
Dhirajsingh212 <dhirajsingh0229@gmail.com>
2024-11-04 17:02:45 +0530

    minor updates

--- web/client/app/(dashboard)/github/[...id]/loading.tsx
+++ web/client/app/(dashboard)/github/[...id]/loading.tsx
@@ -7,18 +7,9 @@ const loading = () => {
       <Tabs defaultValue='files' className='w-full sm:px-4'>
         <TabsList className='mb-4 w-full justify-start gap-0 rounded-none border-b bg-inherit pb-4'>
           <TabsTrigger value='files'>Files</TabsTrigger>
-          <TabsTrigger value='cli'>CLI</TabsTrigger>
           <TabsTrigger value='setting'>Setting</TabsTrigger>
         </TabsList>
         <TabsContent value='files' className='flex flex-col gap-4 sm:px-4'>
-          {Array.from({ length: 10 }).map((_, index) => {
-            return <Skeleton key={index} className='h-40 w-full' />
-          })}
-        </TabsContent>
-        <TabsContent
-          value='cli'
-          className='grid grid-cols-1 gap-4 sm:px-4 md:grid-cols-2 lg:grid-cols-3'
-        >
           {Array.from({ length: 10 }).map((_, index) => {
             return <Skeleton key={index} className='h-40 w-full' />
           })}
--- web/client/app/(dashboard)/github/[...id]/page.tsx
+++ web/client/app/(dashboard)/github/[...id]/page.tsx
@@ -7,14 +7,10 @@ const Page = async ({ params }: { params: { id: string[] } }) => {
       <Tabs defaultValue='files' className='w-full sm:px-4'>
         <TabsList className='mb-4 w-full justify-start gap-0 rounded-none border-b bg-inherit pb-4'>
           <TabsTrigger value='files'>Files</TabsTrigger>
-          <TabsTrigger value='cli'>CLI</TabsTrigger>
           <TabsTrigger value='setting'>Setting</TabsTrigger>
         </TabsList>
         <TabsContent value='files' className='flex flex-col gap-4 sm:px-4'>
           Files
-        </TabsContent>
-        <TabsContent value='cli' className='flex flex-col gap-4 sm:px-4'>
-          CLI
         </TabsContent>
         <TabsContent value='setting' className='px-4'>
           <SettingsComp />
